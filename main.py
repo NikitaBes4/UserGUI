54133f994e8349c9ef771bb32ad761b4623de354
NikitaBes4 <mandarin215@gmail.com>
2024-06-10 09:27:39 +0000

    10.06

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 from nicegui import ui
 
-#!/usr/bin/env python3
 import pandas as pd
 from pandas.api.types import is_bool_dtype, is_numeric_dtype
 
 from nicegui import ui
 
-# df = pd.DataFrame(data={
-#     'col1': [x for x in range(4)],
-#     'col2': ['This', 'column', 'contains', 'strings.'],
-#     'col3': [x / 4 for x in range(4)],
-#     'col4': [True, False, True, False],
-# })
-
 url1="https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
 df = pd.read_csv(url1,header=0,names=['Index', 'Height', 'Weight'], usecols = [0,1,2])
-
+df = df.assign(BMI = (df['Weight']* 0.4535) / (df['Height']*0.0254)**2).round(2)
 print(df)
-# df.to_excel('f.xlsx')
 
 def update(*, df: pd.DataFrame, r: int, c: int, value):
     df.iat[r, c] = value
